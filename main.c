9c247f8d9b4768813fbc3a90e5381392e27cff9d
Sara-sms <sms.saramachado@gmail.com>
2026-05-17 23:46:26 -0300

    feature/contato

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
-// Atividade 1.2 - Nível básico
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    
+    char email [100];
+    char github[50];
+
+
+    printf("Digite seu email:\n");
+    fgets(email, 50, stdin); 
+
+    printf("Digite o user do seu github:\n");
+    fgets(github, 50, stdin); 
+
+    
+    printf("[[[ CONTATO USUARIO ]]]");
+    printf("\nNOME: %s", email);
+    printf("CURSO: %s", github);
+
+    
+
+    return 0;
+}
