8c4352e6ca86e6738a833e377c20c7e82636ba36
Sara-sms <sms.saramachado@gmail.com>
2026-05-17 23:41:02 -0300

    feature/sobre

--- main.c
+++ main.c
@@ -1,1 +1,30 @@
-// Atividade 1.2 - Nível básico
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    
+    char nome [100];
+    char curso[50];
+    int idade = 0;
+
+    printf("Digite seu nome:\n");
+    fgets(nome, 50, stdin); 
+    
+    printf("Digite o nome do seu curso:\n");
+    fgets(curso, 50, stdin); 
+
+    printf("Digite sua idade:\n");
+    scanf("%d", &idade);
+
+
+    
+    printf("[[[ SOBRE USUARIO ]]]");
+    printf("\nNOME: %s", nome);
+    printf("CURSO: %s", curso);
+    printf("IDADE %d\n", idade);
+
+    
+
+    return 0;
+}
